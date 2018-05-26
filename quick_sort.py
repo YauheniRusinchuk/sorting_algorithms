7fa8a0db15286013625ecbcbce0df3fba245e8c7
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2018-05-26 11:19:07 +0300

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -40,3 +40,12 @@ def quick_selection(x, first, last):
 				minIndex = j
 		if minIndex != i:
 			x[i], x[minIndex] = x[minIndex], x[i]
+			
+			
+			
+			
+def qsort(arr): 
+     if len(arr) <= 1:
+          return arr
+     else:
+          return qsort([x for x in arr[1:] if x<arr[0]]) + [arr[0]] + qsort([x for x in arr[1:] if x>=arr[0]]			
