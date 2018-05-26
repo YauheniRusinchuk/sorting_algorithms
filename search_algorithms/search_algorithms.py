a634ec3950936245fb080d2bc7bc988f1dabbd02
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2018-05-26 08:49:29 +0300

    Update search_algorithms.py

--- search_algorithms/search_algorithms.py
+++ search_algorithms/search_algorithms.py
@@ -45,3 +45,52 @@ def binary_search2(value, items, low=0, high=None):
     else:
         assert items[pos] > value
         return binary_search(value, items, low, pos)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   # Linear Search
+def linear_search(data, target):	
+	for i in range(len(data)):
+		if data[i] == target:
+			return True
+	return False
+
+# Iterative Binary Search 
+def binary_search_iterative(data, target):
+	low = 0
+	high = len(data) - 1
+
+	while low <= high:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return False 
+
+# Recursive Binary Search 
+def binary_search_recursive(data, target, low, high):
+	if low > high:
+		return False
+	else:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			return binary_search_recursive(data, target, low, mid-1)
+		else:
+			return binary_search_recursive(data, target, mid+1, high) 
+    
+    
